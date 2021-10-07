fc3f3dc6e02630cebb03459315c63e606887da55
Simon Wikstrand <simon.wikstrand@hey.com>
2021-10-07 16:55:02 +0200

    remove log of key

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,7 +1,6 @@
 import admin from 'firebase-admin';
 import { variables } from '$lib/variables';
 
-console.log(variables.firestore.serviceAccount);
 const db = admin.initializeApp({
 	credential: admin.credential.cert(JSON.parse(variables.firestore.serviceAccount)),
 	projectId: variables.firestore.projectId
